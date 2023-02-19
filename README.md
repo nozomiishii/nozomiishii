e2528c6c5e00f69bc2ade321cdf33ba0c482041d
Nozomi Ishii <nozomiishii.jp@gmail.com>
2023-02-20 01:44:22 +0900

    feat: migrate profile to cv

--- README.md
+++ README.md
@@ -1,37 +1,7 @@
-# cv
+# 🦕 Nozomi Ishii
 
-<!-- Main Image -->
-<br>
-<div align="center">
-  <img src="https://media.giphy.com/media/SabSYEpsVh0di/giphy.gif" alt="thinking" width="480" />
-</div>
-<br>
+![BackGround Image](./images/background.png)
 
-<!-- shields -->
-<div align="center">
-  <a target="_blank" href="https://open.vscode.dev/nozomiishii/cv">
-    <img alt="Open in VSCode" src="https://img.shields.io/static/v1?logo=visualstudiocode&label=&message=Open%20in%20VSCode&labelColor=2c2c32&color=007acc&logoColor=007acc">
-  </a>
-  <a target="_blank" href="https://twitter.com/nozomiishii_dev">
-    <img alt="twitter" src="https://img.shields.io/twitter/follow/nozomiishii_dev?style=social&label=Follow">
-  </a>
-</div>
-<br>
+## 職務経歴書 | CV
 
-## CV | 職務経歴書
-
-- [日本語](https://github.com/nozomiishii/cv/blob/main/src/cv-ja.md)
-
-## Getting Started
-
-Install node_modules
-
-```sh
-yarn install
-```
-
-Activate Lefthook(git hooks)
-
-```sh
-yarn prepare
-```
+[日本語](https://github.com/nozomiishii/cv/blob/main/src/cv-ja.md)
---
+++ scripts/setup.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+set -Ceu
+
+PROJECT_NAME="nozomiishii/nozomiishii"
+username=$(whoami)
+
+printf "\n"
+echo "🎉Hey ${username}, Welcome to ${PROJECT_NAME:-our project}!!"
+
+printf "\n\n⏳ Initialization in progress...\n\n"
+
+# ----------------------------------------------------------------
+# Node.js Corepack
+# ----------------------------------------------------------------
+if ! type node > /dev/null 2>&1; then
+  echo "ERROR: Nodejs must be installed🥲"
+  exit 1
+fi
+
+printf "🐉: Enable corepack...\n"
+corepack enable
+corepack enable npm
+yarn -v
+printf "Enable corepack completed\n\n"
+
+printf "🐉: Install Node modules...\n"
+yarn install
+printf "Install Node modules completed\n\n"
+
+# ----------------------------------------------------------------
+# Lefthook
+# ----------------------------------------------------------------
+if [ ! -f lefthook-local.yaml ]; then
+  printf "🥊: Setup lefthook...\n"
+  npx -y lefthook install
+  touch lefthook-local.yaml
+  printf "Setup lefthook completed\n\n"
+fi
+
+# ----------------------------------------------------------------
+# Env
+# ----------------------------------------------------------------
+# if [ ! -f .env.local ]; then
+#   printf "🏔: Copy env file...\n"
+#   cp .env.example .env.local
+#   printf "Copy env file completed\n\n"
+# fi
+
+# ----------------------------------------------------------------
+# VSCode Preferences
+# ----------------------------------------------------------------
+# if [ ! -f .vscode/settings.json ]; then
+#   printf "🐟: Copy VSCode Preferences...\n"
+#   cp .vscode/settings.sample.jsonc .vscode/settings.json
+#   printf "Copy VSCode Preferences completed\n\n"
+# fi
+
+# ----------------------------------------------------------------
+# Git
+# ----------------------------------------------------------------
+printf "😼: Setup git config...\n"
+git config core.ignorecase false
+printf "Setup git config completed\n\n"
+
+printf "\n🚀Awesome!! Initialization of the development environment is complete!\n\n"
